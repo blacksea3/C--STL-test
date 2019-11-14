914c9afd20f577b60b5f8c8b222f47fe0a0bea79
XianTao Jia <17865191779@163.com>
2019-11-14 21:45:09 +0800

    增加后缀数组=》LCP数组，两者合起来-》后缀树，并且遍历后缀树获得排序后缀字符串

--- other/CppAlgs/String/SuffixArray.cpp
+++ other/CppAlgs/String/SuffixArray.cpp
@@ -97,14 +97,183 @@ std::vector<size_t> BuildSuffixArray(std::string raw)
 		classes = UpdateClasses(L, order, classes);
 		L *= 2;
 	}
-	order.erase(order.begin());
+	//order.erase(order.begin());
 	return order;
+}
+
+/*
+ * 求相邻后缀字符串的LCP
+ * 输入: std::vector<size_t> s 字符串, size_t i和j 位置， equal已知的LCP一定大于等于这个值
+ * 输出int lcp
+ */
+int LCPOfSuffixs(std::string s, size_t i, size_t j, int equal)
+{
+	int lcp = max(0, equal);
+	while ((i + lcp) < s.size() && (j + lcp) < s.size())
+	{
+		if (s[i + lcp] == s[j + lcp]) lcp++;
+		else break;
+	}
+	return lcp;
+}
+
+/*
+ * 反转后缀数组
+ * 输入: std::vector<size_t> order 后缀数组
+ * 输出反转后的后缀数组，后缀规则看代码
+ */
+std::vector<size_t> InvertSuffixArray(std::vector<size_t> order)
+{
+	std::vector<size_t> pos(order.size());
+	for (size_t i = 0; i < order.size(); ++i) pos[order[i]] = i;
+	return pos;
+}
+
+/*
+ * 生成LCP数组
+ * 输入: std::string s字符串，std::vector<size_t> order 后缀数组
+ */
+std::vector<int> ComputeLCPArray(std::string s, std::vector<size_t> order)
+{
+	size_t sSize = s.size();
+	std::vector<int> lcpArray(sSize - 1); //每两个相邻的后缀数组的LCP，放入此数组
+	int lcp = 0;
+	std::vector<size_t> posInOrder = InvertSuffixArray(order);
+	size_t suffix = order[0];
+	for (int i = 0; i < sSize; ++i)
+	{
+		//先求相邻后缀字符串的首字符位置
+		size_t orderIndex = posInOrder[suffix];
+		if (orderIndex == sSize - 1)  //到末尾了，更新sufix，重置lcp
+		{
+			lcp = 0;
+			suffix = (suffix + 1) % sSize;
+			continue;
+		}
+		size_t nextSuffix = order[orderIndex + 1];
+		//通过上一次lcp求这一次的
+		lcp = LCPOfSuffixs(s, suffix, nextSuffix, lcp - 1);
+		lcpArray[orderIndex] = lcp;
+		//更新sufix
+		suffix = (suffix + 1) % sSize;
+	}
+	return lcpArray;
+}
+
+/*
+ * 后缀树节点
+ */
+struct SuffixTreeNode
+{
+	SuffixTreeNode* parent;
+	std::map<char, SuffixTreeNode*> children;
+	int stringDepth;  //根节点到此节点的字符串长度
+	int edgeStart;    //父亲节点到此节点的边在总字符串中的起始下标
+	int edgeEnd;      //父亲节点到此节点的边在总字符串中的结束下标
+	SuffixTreeNode(SuffixTreeNode* p, 
+		std::map<char, SuffixTreeNode*> chi,
+		int sd, int es, int ee) : parent(p), children(chi),
+		stringDepth(sd), edgeStart(es), edgeEnd(ee)
+	{
+		;
+	}
+};
+
+/*
+ * 在当前节点上创键新叶子节点
+ * 输入 SuffixTreeNode* curNode当前节点，
+ * std::string s 字符串，size_t suffix 将要创建的节点对应的后缀字符串的起始下标
+ */
+SuffixTreeNode* CreateNewLeaf(SuffixTreeNode* curNode, std::string s, size_t suffix)
+{
+	SuffixTreeNode* leaf = new SuffixTreeNode(curNode, {}, 
+		s.size() - suffix, suffix + curNode->stringDepth, s.size() - 1);
+	curNode->children[s[leaf->edgeStart]] = leaf;
+	return leaf;
+}
+
+/*
+ * 切边，
+ * 输入 SuffixTreeNode* curNode 边的起始节点，std::string s 字符串
+ * int edgeStart ？，int offset ？
+ * std::string s 字符串，size_t suffix 将要创建的节点对应的后缀字符串的起始下标
+ */
+SuffixTreeNode* BreakEdge(SuffixTreeNode* curNode, std::string s,
+	int edgeStart, int offset)
+{
+	char startChar = s[edgeStart];
+	char midChar = s[edgeStart + offset];
+	SuffixTreeNode* midNode = new SuffixTreeNode(curNode, {},
+		curNode->stringDepth + offset, edgeStart, edgeStart + offset - 1);
+	midNode->children[midChar] = curNode->children[startChar];
+	curNode->children[startChar]->parent = midNode;
+	curNode->children[startChar]->edgeStart += offset;
+	curNode->children[startChar] = midNode;
+	return midNode;
+}
+
+/*
+ * 后缀数组转后缀树
+ * 输入std::string s 字符串, std::vector<size_t> order后缀数组
+ * std::vector<int> lcpArray lcp数组
+ * 输出后缀树根节点
+ */
+SuffixTreeNode* STFromSA(std::string s, std::vector<size_t> order,
+	std::vector<int> lcpArray)
+{
+	SuffixTreeNode* root = new SuffixTreeNode(nullptr, {}, 0, -1, -1);
+	int lcpPrev = 0;
+	SuffixTreeNode* curNode = root;
+	for (int i = 0; i < s.size(); ++i)
+	{
+		size_t suffix = order[i];
+		while (curNode->stringDepth > lcpPrev)
+			curNode = curNode->parent;
+		if (curNode->stringDepth == lcpPrev) //直接新建节点
+			curNode = CreateNewLeaf(curNode, s, suffix);
+		else //需要切边
+		{
+			int edgeStart = order[i - 1] + curNode->stringDepth;
+			int offset = lcpPrev - curNode->stringDepth;
+			SuffixTreeNode* midNode = BreakEdge(curNode, s, edgeStart, offset);
+			curNode = CreateNewLeaf(midNode, s, suffix);
+		}
+		if (i < (s.size() - 1))
+			lcpPrev = lcpArray[i];
+	}
+	return root;
+}
+
+void dfsSuffixTree(std::string& raw, 
+	std::vector<std::string>& total, SuffixTreeNode* pren,
+	std::string pres)
+{
+	pres += raw.substr(pren->edgeStart, pren->edgeEnd - pren->edgeStart + 1);
+	if (pren->children.empty()) total.emplace_back(pres);
+	else
+	{
+		for (auto& c : pren->children)
+			dfsSuffixTree(raw, total, c.second, pres);
+	}
 }
 
 void SuffixArrayTest()
 {
-	std::vector<size_t> res = BuildSuffixArray("ababaa");
+	std::string raw = "ababaa";
+
+	std::vector<size_t> order = BuildSuffixArray(raw);
 	std::cout << "The suffix of ababaa is: ";
-	for (auto& r : res) std::cout << r << " ";
+	for (auto& r : order) std::cout << r << " ";
 	std::cout << std::endl;
+
+	std::cout << "The LCPArray of ababaa is: ";
+	std::vector<int> lcpa = ComputeLCPArray(raw + "$", order);
+	for (auto& r : lcpa) std::cout << r << " ";
+	std::cout << std::endl;
+
+	SuffixTreeNode* root = STFromSA(raw + "$", order, lcpa);
+	std::vector<std::string> total;
+	for (auto& c:root->children) dfsSuffixTree(raw, total, c.second, "");
+	std::cout << "The suffix strings of ababaa is:\n";
+	for (auto& t : total) std::cout << t << std::endl;
 }
